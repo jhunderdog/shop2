a0ca6d8abf387ed97d18b17c924f01fe1d5a0013
jhunderdog <33382water@gmail.com>
2020-10-06 18:24:43 +0900

    Landing Page with Cards

--- server/routes/product.js
+++ server/routes/product.js
@@ -38,4 +38,13 @@ router.post("/", (req, res) => {
     return res.status(200).json({ success: true });
   });
 });
+
+router.post("/products", (req, res) => {
+  Product.find()
+    .populate("writer")
+    .exec((err, productInfo) => {
+      if (err) return res.status(400).json({ success: false, err });
+      return res.status(200).json({ success: true, productInfo });
+    });
+});
 module.exports = router;
