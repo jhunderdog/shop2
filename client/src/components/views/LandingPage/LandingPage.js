5feac005d8b9b0ddb5fbbdd6511c67e9a3bd5dd9
jhunderdog <33382water@gmail.com>
2020-10-09 14:33:57 +0900

    LoadMore

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -7,16 +7,42 @@ import ImageSlider from "../../utils/ImageSlider";
 
 function LandingPage() {
   const [Products, setProducts] = useState([]);
+  const [Skip, setSkip] = useState(0);
+  const [Limit, setLimit] = useState(8);
+  const [PostSize, setPostSize] = useState();
+
   useEffect(() => {
-    axios.post("/api/product/products").then((response) => {
+    let body = {
+      skip: Skip,
+      limit: Limit,
+    };
+    getProducts(body);
+  }, []);
+
+  const getProducts = (body) => {
+    axios.post("/api/product/products", body).then((response) => {
       if (response.data.success) {
-        console.log(response.data);
+        if(body.loadMore) {
+        setProducts([...Products, response.data.productInfo]
+        } else {
         setProducts(response.data.productInfo);
+        }
+        setPostSize(response.data.PostSize)
       } else {
         alert("상품들을 가져오는데 실패 했습니다.");
       }
     });
-  }, []);
+  };
+  const loadMoreHandler = () => {
+    let skip = Skip + Limit
+    let body = {
+      skip: Skip,
+      limit: Limit,
+      loadMore: true
+    };
+    getProducts(body)
+    setSkip(skip)
+  }
   const renderCards = Products.map((product, index) => {
     return (
       <Col lg={6} md={8} xs={24} key={index}>
@@ -26,6 +52,7 @@ function LandingPage() {
       </Col>
     );
   });
+
   return (
     <div style={{ width: "75%", margin: "3rem auto" }}>
       <div style={{ textAlign: "center" }}>
@@ -35,9 +62,11 @@ function LandingPage() {
         </h2>
         <Row gutter={[16, 16]}>{renderCards}</Row>
       </div>
-      <div style={{ justifyContent: "center" }}>
-        <button>더보기</button>
+      {PostSize >= Limit &&
+      <div style={{display:"flex", justifyContent: "center" }}>
+        <button onClick={loadMoreHandler}>더보기</button>
       </div>
+      }
     </div>
   );
 }
